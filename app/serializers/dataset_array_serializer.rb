5dcd49d2d654a71f96508e76ceafb4e4ad4dc351
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-04-26 10:19:17 +0200

    Remove unused information from dataset array serializer

--- app/serializers/dataset_array_serializer.rb
+++ app/serializers/dataset_array_serializer.rb
@@ -1,5 +1,5 @@
 class DatasetArraySerializer < ActiveModel::Serializer
-  attributes :id, :provider, :format, :name, :meta
+  attributes :id, :provider, :format, :name
 
   def provider
     object.dateable.try(:provider_txt)
@@ -7,14 +7,5 @@ class DatasetArraySerializer < ActiveModel::Serializer
 
   def format
     object.try(:format_txt)
-  end
-
-  def meta
-    data = {}
-    data['status']     = object.try(:status)
-    data['updated_at'] = object.try(:updated_at)
-    data['created_at'] = object.try(:created_at)
-    data['rows']       = object.try(:row_count)
-    data
   end
 end
