d0b78edc6f0c7a9bb0a0648b92df52ea8d707031
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-05-04 10:21:27 +0200

    Updates for redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,9 @@
 if ENV["REDISCLOUD_URL"]
-  uri = URI.parse(ENV['REDISCLOUD_URL'])
+  uri    = URI.parse(ENV['REDISCLOUD_URL'])
   $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+else
+  host     = ENV.fetch('REDIS_HOST')     { 'localhost' }
+  port     = ENV.fetch('REDIS_PORT')     { 6379 }
+  password = ENV.fetch('REDIS_PASSWORD') { '' }
+  $redis   = Redis.new(host: host, port: port, password: password)
 end
