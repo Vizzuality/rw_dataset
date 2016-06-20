f88152cd77fef796c7f30c1d3e2df5b5c480e561
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-06-20 16:32:39 +0200

    Fixes for tags

--- spec/jobs/tag_service_job_spec.rb
+++ spec/jobs/tag_service_job_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe TagServiceJob, type: :job do
   include ActiveJob::TestHelper
   let!(:body)   { {"tag": {"tags_list": ["123", '456'], "taggable_id": "c547146d-de0c-47ff-a406-5125667fd5e1",
                            "taggable_type": "Dataset", "taggable_slug": nil}} }
-  subject(:job) { described_class.perform_later(body) }
+  subject(:job) { described_class.perform_later('Dataset', body) }
 
   it 'queues the job' do
     expect { job }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
   end
 
   it 'executes perform' do
-    expect(TagService).to receive(:connect_to_service).with(body)
+    expect(TagService).to receive(:connect_to_service).with('Dataset', body)
     perform_enqueued_jobs { job }
   end
 
