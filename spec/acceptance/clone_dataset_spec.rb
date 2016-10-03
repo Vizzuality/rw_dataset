d0d1f188df76a485fac9c767922316da0c4d2d4a
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-10-03 13:47:02 +0200

    Dataset attributes encoding in CamellowerCase

--- spec/acceptance/clone_dataset_spec.rb
+++ spec/acceptance/clone_dataset_spec.rb
@@ -11,11 +11,11 @@ module V1
         let!(:dataset_id) { Dataset.find_by(name: 'cartodb test set').id }
 
         it 'Allows to clone cartodb dataset' do
-          post "/datasets/#{dataset_id}/clone", params: {"dataset": {"dataset_url": "http://ec2-52-23-163-254.compute-1.amazonaws.com/query/4?select[]=iso,population&filter=(iso=='ESP','AUS')&aggr_by[]=iso&aggr_func=sum&order[]=-iso"} }
+          post "/datasets/#{dataset_id}/clone", params: {"dataset": {"datasetUrl": "http://ec2-52-23-163-254.compute-1.amazonaws.com/query/4?select[]=iso,population&filter=(iso=='ESP','AUS')&aggrBy[]=iso&aggrFunc=sum&order[]=-iso"} }
 
           expect(status).to eq(201)
-          expect(json_attr['name']).to                         match('_copy')
-          expect(json_attr['provider']).to                     eq('rwjson')
+          expect(json_attr['name']).to                       match('_copy')
+          expect(json_attr['provider']).to                   eq('rwjson')
           expect(json_attr['clonedHost']['hostType']).to     eq('RestConnector')
           expect(json_attr['clonedHost']['hostProvider']).to eq('cartodb')
         end
@@ -26,14 +26,14 @@ module V1
         let!(:settings)   { ServiceSetting.create(name: 'api-gateway', listener: true, token: '3123123der324eewr434ewr4324', url: 'http://192.168.99.100:8000') }
 
         it 'Allows to clone json dataset' do
-          post "/datasets/#{dataset_id}/clone", params: {"dataset": {"dataset_url": "/query/4?select[]=iso,population&filter=(iso=='ESP','AUS')&aggr_by[]=iso&aggr_func=sum&order[]=-iso"} }
+          post "/datasets/#{dataset_id}/clone", params: {"dataset": {"datasetUrl": "/query/4?select[]=iso,population&filter=(iso=='ESP','AUS')&aggrBy[]=iso&aggrFunc=sum&order[]=-iso"} }
 
           expect(status).to eq(201)
-          expect(json_attr['name']).to                         match('_copy')
-          expect(json_attr['provider']).to                     eq('rwjson')
+          expect(json_attr['name']).to                       match('_copy')
+          expect(json_attr['provider']).to                   eq('rwjson')
           expect(json_attr['clonedHost']['hostType']).to     eq('JsonConnector')
           expect(json_attr['clonedHost']['hostProvider']).to eq('rwjson')
-          expect(json_attr['clonedHost']['hostUrl']).to      eq("http://192.168.99.100:8000/query/4?select[]=iso,population&filter=(iso=='ESP','AUS')&aggr_by[]=iso&aggr_func=sum&order[]=-iso")
+          expect(json_attr['clonedHost']['hostUrl']).to      eq("http://192.168.99.100:8000/query/4?select[]=iso,population&filter=(iso=='ESP','AUS')&aggrBy[]=iso&aggrFunc=sum&order[]=-iso")
         end
       end
     end
