0bda4f9b1618c9e823f18ae86439a352b0031065
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-10-26 16:47:53 +0200

    Code clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,15 @@ class User < ActiveHash::Base
     user_role    = user.role
     user_apps    = user.apps if user.apps.present?
     dataset_apps = dataset_apps.uniq if dataset_apps.present?
-    any_apps     = if user_apps.present? && dataset_apps.present? && (user_apps & dataset_apps).any? && user_apps.size > dataset_apps.size && user_apps | dataset_apps == user_apps
+    first_case   = user_apps.present? && dataset_apps.present? &&
+                                      (user_apps & dataset_apps).any? &&
+                                      user_apps.size > dataset_apps.size &&
+                                      user_apps | dataset_apps == user_apps
+    second_case  = user_apps.present? && dataset_apps.present? &&
+                                      user_apps.sort == dataset_apps.sort
+    any_apps     = if first_case
                      true
-                   elsif user_apps.present? && dataset_apps.present? && user_apps.sort == dataset_apps.sort
+                   elsif second_case
                      true
                    else
                      false
@@ -21,18 +27,26 @@ class User < ActiveHash::Base
     case user_role
     when 'user' then false
     when 'manager'
-      if any_apps.present? && (user_dataset || dataset_user_id.blank?)
-        true
-      else
-        false
-      end
+      manager_ability(any_apps, user_dataset, dataset_user_id)
     when 'admin'
-      if any_apps.present?
-        true
-      else
-        false
-      end
+      admin_ability(any_apps)
     when 'superadmin'
+      true
+    else
+      false
+    end
+  end
+
+  def self.manager_ability(any_apps, user_dataset, dataset_user_id)
+    if any_apps.present? && (user_dataset || dataset_user_id.blank?)
+      true
+    else
+      false
+    end
+  end
+
+  def self.admin_ability(any_apps)
+    if any_apps.present?
       true
     else
       false
