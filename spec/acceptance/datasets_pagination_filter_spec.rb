69aee53cf4e0a0c91066bbf35fe4009ca0af9696
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-11-17 16:49:25 +0100

    Add pagination and sort params to datasets list

---
+++ spec/acceptance/datasets_pagination_filter_spec.rb
@@ -0,0 +1,189 @@
+require 'acceptance_helper'
+
+module V1
+  describe 'Datasets', type: :request do
+    fixtures :rest_connectors
+    fixtures :json_connectors
+    fixtures :wms_connectors
+    fixtures :doc_connectors
+    fixtures :datasets
+
+    context 'For datasets list', redis: true do
+      it 'Allows to access datasets list without filtering' do
+        get '/dataset'
+
+        dataset_json = json[0]['attributes']
+        dataset_rest = json[3]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(10)
+        expect(dataset_json['provider']).to eq('rwjson')
+        expect(dataset_rest['provider']).to eq('cartodb')
+      end
+
+      it 'Allows to access datasets list filtering by type rest' do
+        get '/dataset?connectorType=rest'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(7)
+        expect(dataset['provider']).to      eq('cartodb')
+        expect(dataset['connectorType']).to eq('rest')
+      end
+
+      it 'Allows to access datasets list filtering by type json' do
+        get '/dataset?connectorType=json'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(2)
+        expect(dataset['provider']).to      eq('rwjson')
+        expect(dataset['connectorType']).to eq('json')
+      end
+
+      it 'Allows to access datasets list filtering by provider rwjson' do
+        get '/dataset?provider=rwjson'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(2)
+        expect(dataset['provider']).to      eq('rwjson')
+        expect(dataset['connectorType']).to eq('json')
+      end
+
+      it 'Allows to access datasets list filtering by provider cartodb' do
+        get '/dataset?provider=cartodb'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(6)
+        expect(dataset['provider']).to      eq('cartodb')
+        expect(dataset['connectorType']).to eq('rest')
+      end
+
+      it 'Allows to access datasets list filtering by provider featureservice' do
+        get '/dataset?provider=featureservice'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(1)
+        expect(dataset['provider']).to      eq('featureservice')
+        expect(dataset['connectorType']).to eq('rest')
+      end
+
+      it 'Allows to access datasets list filtering by provider csv' do
+        get '/dataset?provider=csv'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(1)
+        expect(dataset['provider']).to      eq('csv')
+        expect(dataset['connectorType']).to eq('document')
+      end
+
+      it 'Allows to access datasets list filtering by provider wms' do
+        get '/dataset?provider=wms'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(1)
+        expect(dataset['provider']).to      eq('wms')
+        expect(dataset['connectorType']).to eq('wms')
+      end
+
+      it 'Allows to access datasets list filtering by non existing provider' do
+        get '/dataset?provider=nonexisting'
+
+        expect(status).to eq(200)
+        expect(json.length).to eq(0)
+      end
+
+      it 'Allows to access datasets list filtering by all providers' do
+        get '/dataset?provider=all'
+
+        expect(status).to eq(200)
+        expect(json.length).to eq(10)
+      end
+
+      it 'Allows to access datasets list filtering by type wms' do
+        get '/dataset?connectorType=wms'
+
+        dataset = json[0]['attributes']
+        expect(status).to eq(200)
+        expect(json.length).to              eq(1)
+        expect(dataset['provider']).to      eq('wms')
+        expect(dataset['connectorType']).to eq('wms')
+      end
+
+      it 'Show list of all datasets using status filter all' do
+        get '/dataset?status=all'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(10)
+      end
+
+      it 'Show list of datasets with pending status' do
+        get '/dataset?status=pending'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(1)
+      end
+
+      it 'Show list of datasets with active status' do
+        get '/dataset?status=active'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(10)
+      end
+
+      it 'Show list of datasets with disabled status' do
+        get '/dataset?status=disabled'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(1)
+      end
+
+      it 'Show list of datasets for app GFW' do
+        get '/dataset?app=GFw'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(10)
+        expect(json[0]['attributes']['application'][0]).to eq('gfw')
+      end
+
+      it 'Show list of datasets for app WRW' do
+        get '/dataset?app=wrw'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(1)
+      end
+
+      it 'Show blank list of datasets for not existing app' do
+        get '/dataset?app=notexisting'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(0)
+      end
+
+      it 'Show blank list of datasets for all apps' do
+        get '/dataset?app=all'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(10)
+      end
+
+      it 'Show blank list of datasets for all apps and second page (for total items 13)' do
+        get '/dataset?page[number]=2&page[size]=10&status=all'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(3)
+      end
+
+      it 'Show blank list of datasets for all apps first page' do
+        get '/dataset?page[number]=1&page[size]=10&status=all'
+
+        expect(status).to eq(200)
+        expect(json.size).to eq(10)
+      end
+    end
+  end
+end
